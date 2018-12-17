4424db852a27b813a2714a95b0f6875d5ce09adc
SelcukAtes <sselcukates@gmail.com>
2018-12-16 23:06:51 -0800

    added first render of the Header Component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Bird from './containers/Bird/Bird';
 import Login from './containers/Login/Login';
 import Tweet from './components/Tweet/Tweet';
-import Profile from './components/Profile/Profile'
+import Profile from './components/Profile/Profile';
+import Headers from './components/Header/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Headers />
         <Bird />
         <Login />
         <Tweet />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import classes from './Header.module.css';
+
+class Header extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        This is Header
+      </div>
+    )
+  }
+}
+
+export default Header;
