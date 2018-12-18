bd431a2e476b1b5897970c0263355dfd7fe3cea9
SelcukAtes <sselcukates@gmail.com>
2018-12-17 16:50:01 -0800

    syntax error fixed for Message components

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 
 import classes from './Messages.module.css';
 
-class Messages extend Component {
+class Messages extends Component {
   state = {
     hello: ''
   }
-  render{
+  render() {
     return (
       <div>
-        <p>This is from</p>
-        <div>{This is from coming}</div>
+        <p>We are going to display all the user Messages in here</p>
       </div>
     )
   }
